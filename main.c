941ca32838d4678786b747d3b0d3907dd658fa70
pdlmn <jseldardw@gmail.com>
2024-03-11 11:04:48 +0100

    feat: added parsing of the input

--- main.c
+++ main.c
@@ -6,48 +6,82 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:14:04 by emuminov          #+#    #+#             */
-/*   Updated: 2024/03/08 16:59:57 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/03/11 09:55:12 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
 
-pthread_mutex_t	mutex;
-int mails = 0;
+typedef	pthread_mutex_t t_fork;
 
-void	*roll_dice()
+typedef struct	s_philo
 {
-	for (int i = 0; i < 10000; i++)
-	{
-		pthread_mutex_lock(&mutex);
-		mails++;
-		pthread_mutex_unlock(&mutex);
-	}
-	return (NULL);
+	long	index;
+	long	last_eat_time;
+	long	meals_counter;
+	int		state;
+	t_fork	left_fork;
+	t_fork	*right_fork;
+}				t_philo;
+
+typedef struct	s_params
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	max_nbr_of_meals;
+	t_philo	*philos;
+}				t_params;
+
+void	terminate(char *msg)
+{
+	if (msg)
+		printf("%s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+void	params_init(int argc, char **argv, t_params *params)
+{
+	params->philo_nbr = atol(argv[1]);
+	params->time_to_die = atol(argv[2]);
+	params->time_to_eat = atol(argv[2]);
+	params->time_to_sleep = atol(argv[3]);
+	if (argc == 5)
+		params->max_nbr_of_meals = -1;
+	if (argc == 6)
+		params->max_nbr_of_meals = atol(argv[5]);
+}
+
+void	params_validate(t_params *params)
+{
+	if (params->philo_nbr <= 0 || params->philo_nbr > INT_MAX)
+		terminate("Wrong number of philosophers\n");
+	else if (params->time_to_die <= 0 || params->time_to_die > INT_MAX)
+		terminate("Bad time to die\n");
+	else if (params->time_to_eat <= 0 || params->time_to_eat > INT_MAX)
+		terminate("Bad time to eat\n");
+	else if (params->time_to_sleep <= 0 || params->time_to_sleep > INT_MAX)
+		terminate("Bad time to sleep\n");
+}
+
+void	parse_input(int argc, char **argv, t_params *params)
+{
+	params_init(argc, argv, params);
+	params_validate(params);
+	
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t		th[4];
-	int				i;
+	t_params	params;
 
-	pthread_mutex_init(&mutex, NULL);
-	for (i = 0; i < 4; i++)
-	{
-		if (pthread_create(&th[i], NULL, routine1, NULL) != 0)
-			return (EXIT_FAILURE);
-		printf("Thread %d has been started\n", i);
-	}
-	for (i = 0; i < 4; i++)
-	{
-		if (pthread_join(th[i], NULL))
-			return (EXIT_FAILURE);
-		printf("Thread %d has finished execution\n", i);
-	}
-	pthread_mutex_destroy(&mutex);
-	printf("Number of mails: %d\n", mails);
+	if (argc < 4 || argc > 6)
+		exit(EXIT_FAILURE);
+	parse_input(argc, argv, &params);
 	return (EXIT_SUCCESS);
 }
