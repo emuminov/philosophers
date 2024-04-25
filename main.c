3275b8fc36173b9a2b184d6443370765e0129836
Eldar Muminov <emuminov@z4r3p7.42lyon.fr>
2024-04-25 17:42:32 +0200

    feat: made simulation stop on eating max number of meals

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: emuminov <emuminov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:14:04 by emuminov          #+#    #+#             */
-/*   Updated: 2024/04/25 17:07:58 by emuminov         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:40:16 by emuminov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -304,6 +304,7 @@ void	eat(t_philo *philo)
 		write_status(philo, EAT, 0);
 		pthread_mutex_lock(&philo->meal_lock);
 		philo->last_meal_time = get_time();
+		philo->meals_counter++;
 		pthread_mutex_unlock(&philo->meal_lock);
 		ft_usleep(philo->params->time_to_eat);
 	}
@@ -369,7 +370,6 @@ int	check_if_philo_should_be_dead(t_philo *philo, t_params *p)
 	pthread_mutex_lock(&philo->meal_lock);
 	last_meal_time_diff = get_time() - philo->last_meal_time;
 	pthread_mutex_unlock(&philo->meal_lock);
-	// printf("%u philo ate %lu ms ago\n", philo->index, last_meal_time_diff);
 	if (last_meal_time_diff >= p->time_to_die)
 		return (1);
 	return (0);
@@ -389,6 +389,32 @@ void	stop_program(t_params *p)
 		i++;
 	}
 	exit(EXIT_SUCCESS);
+}
+
+unsigned int	get_or_set_meals_counter(t_philo *philo, int val, enum e_flag f)
+{
+	unsigned int	meals_counter;
+
+	pthread_mutex_lock(&philo->meal_lock);
+	if (f == SET)
+		philo->meals_counter = val;
+	meals_counter = philo->meals_counter;
+	pthread_mutex_unlock(&philo->meal_lock);
+	return (meals_counter);
+}
+
+int	check_if_philos_ate_all_their_meals(t_params *p)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < p->philo_nbr)
+	{
+		if (get_or_set_meals_counter(&p->philos[i], -1, GET) < p->max_nbr_of_meals)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 void	*monitor_routine(void *data)
@@ -411,7 +437,14 @@ void	*monitor_routine(void *data)
 				return (NULL);
 			}
 			i++;
-			ft_usleep(2);
+			ft_usleep(1);
+		}
+		if (check_if_philos_ate_all_their_meals(p))
+		{
+			pthread_mutex_lock(&p->sync_lock);
+			p->is_running = 0;
+			pthread_mutex_unlock(&p->sync_lock);
+			return (NULL);
 		}
 	}
 	return (NULL);
